f1cae914b9b62bf2e5066f7049b3d1d68710a328
Floron <Floron@bk.ru>
2024-04-11 15:47:45 +0300

    many changes

---
+++ 6. Diplom_FilmsApp/Controllers/mainCollectionViewDataSource.swift
@@ -0,0 +1,12 @@
+//
+//  mainCollectionViewDataSource.swift
+//  6. Diplom_FilmsApp
+//
+//  Created by Floron on 11.04.2024.
+//
+
+import UIKit
+
+class mainCollectionViewDataSource: UICollectionReusableView {
+        
+}
---
+++ 6. Diplom_FilmsApp/Models/ModelForCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  ModelForCollectionView.swift
+//  6. Diplom_FilmsApp
+//
+//  Created by Floron on 11.04.2024.
+//
+
+import Foundation
